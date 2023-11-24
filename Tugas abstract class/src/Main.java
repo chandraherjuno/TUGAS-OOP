31dbd85259b8afdefaff2093add04b8ca7ec063a
chandraherjuno <herjunochandra471@gmail.com>
2023-11-24 19:48:10 +0700

    push tugas

--- Tugas abstract class/src/Main.java
+++ Tugas abstract class/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-      
+        BaseInvoice yogya = new BaseInvoice();
+        yogya.kode_invoice = "001";
+        yogya.klasifikasi_produk = "convenience";
+        yogya.nama_supplier = "juno chicken";
+        yogya.waiting_time_pembongkaran = "10 menit";
+        yogya.jumlah_box = "100 box";
+        yogya.estimasi_sampai();
+        yogya.Pengiriman_wilayah();
     }
-    
 }
